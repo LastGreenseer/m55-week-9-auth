ae7e508ed68cb7bb757069ac55aeee40f56d3798
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-15 15:41:16 +0100

    chore: usermodel set up and connected

---
+++ src/user/model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../db/connection");
+
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      alowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+);
+
+module.exports = User;
