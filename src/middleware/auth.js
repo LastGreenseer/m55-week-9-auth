68f0c6263616d9d64550ac454090c0e49262ffb5
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-16 14:12:40 +0100

    push

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -17,28 +17,29 @@ const hashPass = async (req, res, next) => {
   }
 };
 
-const comparePass = (req, res, next) => {
-  const { username, password } = req.body;
+// const comparePass = (req, res, next) => {
+//   const { username, password } = req.body;
 
-  //bcrypt.compare takes 2 parameters, the plaintext password and the hashed password from the db
+//   bcrypt.compare takes 2 parameters, the plaintext password and the hashed password from the db
 
-  // get user from db with username
-  // check if user exists
-  // compare password
-  //check is return value is true or false
-  // if false response "passwords do not match"
-  // attach user to the request
-  //next
-  try {
-    bcrypt.compare(password, hashedPassword, (err, result) => {
-      if (!password) {
-        return res.status(400).json({ message: "Password does not match" });
-      }
-    });
-  } catch (error) {
-    res.status(500).json()({ message: error.message, error });
-  }
-};
+// //   get user from db with username
+// //   check if user exists
+// //   compare password
+// //   check is return value is true or false
+// //   if false response "passwords do not match"
+// //   attach user to the request
+// //   next
+
+//   try {
+//     bcrypt.compare(password, hashedPassword, (err, result) => {
+//       if (!password) {
+//         return res.status(400).json({ message: "Password does not match" });
+//       }
+//     });
+//   } catch (error) {
+//     res.status(500).json()({ message: error.message, error });
+//   }
+// };
 
 module.exports = {
   hashPass,
