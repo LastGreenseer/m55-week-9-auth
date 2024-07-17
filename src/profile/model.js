3d979eefb0fc6fc8ccabafbc3a0060b43d125486
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-17 14:36:23 +0100

    feature: Profile model created with association to Users

--- src/profile/model.js
+++ src/profile/model.js
@@ -1,2 +1,15 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db/connection");
+
+const Profile = sequelize.define("Profile", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Profile
